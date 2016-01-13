750267bdbebabd693ef70ce5c028de24f7b8dfbd
liubo <liubo.intern@xiaoyezi.com>
2016-01-13 10:48:12 +0800

    modifi gitignore

---
+++ Classes/ShadowLayer.cpp
@@ -0,0 +1,9 @@
+//
+//  ShadowLayer.cpp
+//  DayDayUp
+//
+//  Created by LIUBO on 16/1/13.
+//
+//
+
+#include "ShadowLayer.h"
---
+++ Classes/ShadowLayer.h
@@ -0,0 +1,14 @@
+//
+//  ShadowLayer.h
+//  DayDayUp
+//
+//  Created by LIUBO on 16/1/13.
+//
+//
+
+#ifndef ShadowLayer_h
+#define ShadowLayer_h
+
+#include <stdio.h>
+
+#endif /* ShadowLayer_h */
